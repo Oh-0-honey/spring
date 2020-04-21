e30bb7af2b21d0ae49516322ce1899129e04445d
Hoeny5 <oyh951416@naver.com>
2020-04-22 01:56:30 +0900

    CHAPTER 4 완료

--- src/main/java/com/spring/test/service/posts/PostsService.java
+++ src/main/java/com/spring/test/service/posts/PostsService.java
@@ -27,6 +27,7 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){//쿼리문 없이 Entity 객체의 값만 변경하면 된다. 이 개념을 더티체킹이라고 한다.
         posts posts=postRepository.findById(id).orElseThrow(() ->new IllegalArgumentException("해당 게시물이 없습니다. id="+id));
+        System.out.println(requestDto.getTitle()+"!!!!!!!!!!!!!!!!!!");
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
@@ -39,5 +40,12 @@ public class PostsService {
     @Transactional(readOnly = true)//readOnly를 통한 조회속도 개선
     public List<PostsListResponseDto> findAllDesc(){
         return postRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        posts posts=postRepository.findById(id).orElseThrow(() ->new IllegalArgumentException("해당 게시물이 없습니다. id="+id));
+        postRepository.delete(posts);
+
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -7,6 +7,9 @@ var main = {
         $('#btn-update').on('click',function(){
             _this.update();
         });
+        $('#btn-delete').on('click',function(){
+            _this.delete();
+        });
     },
     save : function(){
         var data = {
@@ -40,9 +43,24 @@ var main = {
             type: 'PUT',
             url : '/api/v1/posts/'+id,//posts 뒤에 슬래쉬 필수!!!
             dataType : 'json',
-            contentType:'application/json; charset=utf-8'
+            contentType:'application/json; charset=utf-8',
+            data:JSON.stringify(data)
         }).done(function(){
                       alert('글이 수정되었습니다.')
+                      window.location.href='/';//성공시 메인페이지로
+                  }).fail(function (error){
+                      alert(JSON.stringify(error));//실패시 알림
+                  });
+    },
+    delete : function(){
+        var id = $('#id').val();
+        $.ajax({
+            type: 'DELETE',
+            url : '/api/v1/posts/'+id,//posts 뒤에 슬래쉬 필수!!!
+            dataType : 'json',
+            contentType:'application/json; charset=utf-8',
+        }).done(function(){
+                      alert('글이 삭제되었습니다.')
                       window.location.href='/';//성공시 메인페이지로
                   }).fail(function (error){
                       alert(JSON.stringify(error));//실패시 알림
